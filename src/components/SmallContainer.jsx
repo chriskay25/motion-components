91bd0179c389013d7f81d966cba0ad7581476793
Chris Kay <ckay84@protonmail.com>
2021-01-07 23:05:31 -0500

    Create SmallContainer component with motion div

---
+++ src/components/SmallContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+const SmallContainer = () => {
+    return (
+        <motion.div>
+            Small Container
+        </motion.div>
+    )
+}
+ 
+export default SmallContainer
