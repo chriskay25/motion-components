cf2deea76748c4da28f96d434f498a89a4578d25
Chris Kay <ckay84@protonmail.com>
2021-01-09 13:16:46 -0500

    Add variants to SmallContainer

--- src/components/SmallContainer.jsx
+++ src/components/SmallContainer.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import ButtonA from './buttons/ButtonA'
+
+const variants = {
+    hidden: {
+        opacity: 0,
+        y: '-100vh'
+    },
+    visible: {
+        opacity: 1,
+        y: 0,
+        backgroundColor: 'lightblue',
+        transition: {
+            duration: 1,
+            type: 'spring',
+            stiffness: 60,
+            ease: 'easeInOut',
+            when: 'beforeChildren'
+        },
+    }
+}
 
 const SmallContainer = () => {
     return (
-        <motion.div>
-            Small Container
+        <motion.div 
+            className="small-container"
+            variants={variants}
+            initial='hidden'
+            animate='visible'
+        >
+            <ButtonA />
         </motion.div>
     )
 }
