9fd47b2cd42540df67f96d2a7429d093821cc9e0
Chris Kay <ckay84@protonmail.com>
2021-01-09 13:16:32 -0500

    Add variants to Container

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import SmallContainer from './SmallContainer'
+
+const variants = {
+    hidden: {
+        opacity: 0,
+    },
+    visible: {
+        opacity: 1,
+        transition: {
+            duration: 1
+        }
+    }
+}
 
 const Container = () => {
     return (
-        <motion.div className='container'>
-            Container
+        <motion.div 
+            className='container'
+            variants={variants}
+            initial='hidden'
+            animate='visible'
+        >
+            <SmallContainer />
         </motion.div>
     )
 }
